4cfdb0fec9616b008521f0bcfdcc63a5ac7be035
Prince Bolarin <idevmac.info@gmail.com>
2025-10-20 16:17:01 +0100

    348ehrh3u

--- components/Button.jsx
+++ components/Button.jsx
@@ -3,22 +3,20 @@ import React from 'react'
 
 function Button({onClick, type, disabled, loading, title}) {
   return (
-    <div>
-      <button 
-          type={type} 
-          onClick={onClick}
-          disabled={disabled}
-          className='btn bg-main-100 hover:bg-main-100 hover:shadow-lg shadow-gray-400 w-full mx-auto text-white font-semibold'
-      >
-          {
-              {loading} 
-              ?
-              <Loader/>
-              :
-              {title}
-          }
-      </button>
-    </div>
+    <button 
+        type={type} 
+        onClick={onClick}
+        disabled={disabled}
+        className='btn bg-main-100 hover:bg-main-100 hover:shadow-lg shadow-gray-400 w-full mx-auto text-white font-semibold'
+    >
+      {
+        loading
+        ?
+        <Loader className='animate-spin'/>
+        :
+        title
+      }
+    </button>
   )
 }
 
