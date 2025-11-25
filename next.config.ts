9c3cdaefee8c314d23a690718256e20389e5541c
Prince Bolarin <idevmac.info@gmail.com>
2025-11-25 13:44:15 +0100

    much changes made ----

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,5 @@
 import type { NextConfig } from "next";
 
-// module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         source: "/api/:path*",
-//         headers: [
-//           {
-//             key: "Access-Control-Allow-Origin",
-//             value: "*", // Set your origin
-//           },
-//           {
-//             key: "Access-Control-Allow-Methods",
-//             value: "GET, POST, PUT, DELETE, OPTIONS",
-//           },
-//           {
-//             key: "Access-Control-Allow-Headers",
-//             value: "Content-Type, Authorization",
-//           },
-//         ],
-//       },
-//     ]
-//   },
-// }
 
 const nextConfig: NextConfig = {
   /* config options here */
